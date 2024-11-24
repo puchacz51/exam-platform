4cb9f0359fe8f1efe1d7cea63f05f618df07a5dc
Bartosz Sowinski <pochbar@gmail.com>
2024-11-24 17:19:05 +0100

    fix types

--- scripts/migration.ts
+++ scripts/migration.ts
@@ -1,7 +1,12 @@
+
 // import db from '@/lib/db';
 
 // async function main() {
 //   await readMigrationFiles(db, { migrationsFolder: './drizzle' });
 // }
+// async function main() {
+//   await readMigrationFiles(db, { migrationsFolder: './drizzle' });
+// }
 
+// main();
 // main();
